d237384f4cf9f7542042a381ca844254c534841d
Juliette <ajuliette7@gmail.com>
2020-12-14 17:57:08 +0100

    deuxieme page html

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+//EFFET SUR LE TITRE//
+ScrollReveal().reveal('h1', {delay: 200, distance: '40px', scale: 1});
+ScrollReveal().reveal('h2', {delay: 200, distance: '40px', scale: 1});
+
+
+//SIDENAV//
+// function openNav() {
+//     document.getElementById("sideNavigation").style.width = "300px";
+//     document.getElementById("main").style.marginLeft = "300px";
+// }
+ 
+// function closeNav() {
+//     document.getElementById("sideNavigation").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+// }

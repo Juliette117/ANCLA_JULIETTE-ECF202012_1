6c9a71545fdb4bc823b7d712fb8b7f6ce90d6e32
Juliette <ajuliette7@gmail.com>
2020-12-17 11:09:50 +0100

    ajout JS

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,17 @@ ScrollReveal().reveal('h1', {delay: 200, distance: '40px', scale: 1});
 ScrollReveal().reveal('h2', {delay: 200, distance: '40px', scale: 1});
 
 
-// SIDENAV (no fixed)//
+//HISTORIQUE DE RECHERCHE//
+      let i = 0;
+      function add() {   
+      const listItem = document.querySelector("input").value;
+	    const listed = '<ul>' + listItem + '</ul>';
+      document.querySelector("aside").innerHTML += listed;
+      };
+
+
+
+      // // SIDENAV (no fixed)//
 // function openNav() {
 //     document.getElementById("sideNavigation").style.width = "300px";
 //     document.getElementById("main").style.marginLeft = "300px";
@@ -13,3 +23,19 @@ ScrollReveal().reveal('h2', {delay: 200, distance: '40px', scale: 1});
 //     document.getElementById("sideNavigation").style.width = "0";
 //     document.getElementById("main").style.marginLeft = "0";
 // }
+
+
+
+// // Header fixé au scroll (no fixed)
+// window.onscroll = function () {
+        
+//     if(document.documentElement.scrollTop > 70){
+//       document.querySelector("header").style.position = "fixed";
+//       document.querySelector("header").style.top = "0";
+//       document.querySelector("header").style.paddingRight = "1060px";
+//       document.querySelector("header").style.paddingLeft = "1042px"; 
+//     }
+//     else{
+//       document.getElementById("header").style.background = "white";
+//     }
+//   }
